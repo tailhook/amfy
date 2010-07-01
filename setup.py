63ead614d14f414832a3d91f19e6127567e37bc7
pc <pc@a536efce-96a7-49c4-bcf3-cac9190c9430>
2010-07-01 20:11:10 +0000

    `setup.py` file
    git-svn-id: svn://localhost/trunk@3 a536efce-96a7-49c4-bcf3-cac9190c9430

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='Amfy',
+      version='0.1',
+      description='AMF serializer/deserializer for python3',
+      author='Paul Colomiets',
+      author_email='pc@gafol.net',
+      url='http://amfy.gafol.net',
+      classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        ],
+      packages=['amfy'],
+    )
